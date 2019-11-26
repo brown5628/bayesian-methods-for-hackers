587cb469f9b207eee2a9da9813537f6f47466738
Will Brown <brown5628@gmail.com>
2019-11-26 07:39:06 -0500

    Continue ch 2 up to Bayesian A/B testing

--- 02_a_little_more_on_pymc.py
+++ 02_a_little_more_on_pymc.py
@@ -57,3 +57,63 @@ with pm.Model() as theano_test:
 
 # %% codecell
 # Including obs in the model
+
+%matplotlib inline
+from IPython.core.pylabtools import figsize
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+figsize(12.5, 4)
+
+samples = lambda_1.random(size=20000)
+plt.hist(samples, bins=70, density=True, histtype="stepfilled")
+plt.title("Prior distribution for $\lambda_1$")
+plt.xlim(0, 8)
+
+data = np.array([10, 5])
+with model:
+    fixed_variable = pm.Poisson("fxd", 1, observed=data)
+print("value: ", fixed_variable.tag.test_value)
+
+# We're using some fake data here
+data = np.array([10, 25, 15, 20, 35])
+with model:
+    obs = pm.Poisson("obs", lambda_, observed = data)
+print(obs.tag.test_value)
+
+# %% codecell
+#Same story, different ending
+tau = np.random.randint(0,80)
+print(tau)
+
+alpha = 1./20.
+lambda_1, lambda_2 = np.random.exponential(scale=1/alpha, size=2)
+print(lambda_1, lambda_2)
+
+data = np.r_[stats.poisson.rvs(mu=lambda_1, size=tau), stats.poisson.rvs(mu=lambda_2, size=80-tau)]
+
+plt.bar(np.arange(80), data, color="#348ABD")
+plt.bar(tau-1, data[tau - 1], color="r", label="user behaviour changed")
+plt.xlabel("Time (days)")
+plt.ylabel("count of text-msgs received")
+plt.title("Artificial dataset")
+plt.xlim(0, 80)
+plt.legend()
+
+
+def plot_artificial_sms_dataset():
+    tau = stats.randint.rvs(0, 80)
+    alpha = 1./20.
+    lambda_1, lambda_2 = stats.expon.rvs(scale=1/alpha, size=2)
+    data = np.r_[stats.poisson.rvs(mu=lambda_1, size=tau), stats.poisson.rvs(mu=lambda_2, size=80 - tau)]
+    plt.bar(np.arange(80), data, color="#348ABD")
+    plt.bar(tau - 1, data[tau-1], color="r", label="user behaviour changed")
+    plt.xlim(0, 80);
+
+figsize(12.5, 5)
+plt.title("More example of artificial datasets")
+for i in range(4):
+    plt.subplot(4, 1, i+1)
+    plot_artificial_sms_dataset()
+
+# %%
+# Bayesian A/B Testing
