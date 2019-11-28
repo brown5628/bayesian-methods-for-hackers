180e25e75d50bf585001442a6d08ff915ebe5204
Will Brown <brown5628@gmail.com>
2019-11-28 10:47:42 -0500

    Continue ch 2 up to cheating students

--- 02_a_little_more_on_pymc.py
+++ 02_a_little_more_on_pymc.py
@@ -117,3 +117,150 @@ for i in range(4):
 
 # %%
 # Bayesian A/B Testing
+import pymc3 as pm
+
+# The parameters are the bounds of the Uniform.
+with pm.Model() as model:
+    p = pm.Uniform('p', lower = 0, upper = 1)
+
+# set constants
+p_true = 0.05 # remember, this is unknown
+N = 1500
+
+# sample N Bernoulli random variables from Ber(0.05).
+# each random variable has a 0.05 chance of being 1.
+# this is the data-generation step
+occurances = stats.bernoulli.rvs(p_true, size = N)
+
+print(occurances)
+print(np.sum(occurances))
+
+# Occurances.mean is equal to n/N
+print("What is the observed frequency in Group A? %.4f" % np.mean(occurances))
+print("Does this equal the true frequency? %s" % (np.mean(occurances) == p_true))
+
+# include the observations, which are Bernoulli
+with model:
+    obs = pm.Bernoulli("obs", p, observed=occurances)
+    # To be explained in ch 3
+    step = pm.Metropolis()
+    trace = pm.sample(18000, step = step)
+    burned_trace = trace[1000:]
+
+figsize(12.5, 4)
+plt.title("Posterior distribution of $p_A$, the true effectiveness of Site A")
+plt.vlines(p_true, 0, 90, linestyle="--", label="true $p_A (unknown)")
+plt.hist(burned_trace["p"], bins = 25, histtype="stepfilled", normed=True)
+plt.legend()
+
+# A and B together
+import pymc3 as pm
+figsize(12,4)
+
+# these two quantities are unknown to us.
+true_p_A = .05
+true_p_B = .04
+
+# notice the unequal sample sizes -- no problem in Bayesian analysis.
+N_A = 1500
+N_B = 750
+
+# generate some observations
+observations_A = stats.bernoulli.rvs(true_p_A, size = N_A)
+observations_B = stats.bernoulli.rvs(true_p_B, size = N_B)
+print("Obs from Site A: ", observations_A[:30], "...")
+print("Obs from Site B: ", observations_B[:30], "...")
+print(np.mean(observations_A))
+print(np.mean(observations_B))
+
+# Set up the pymc3 model. Again assume Uniform priors for p_A and p_B.
+with pm.Model() as model:
+    p_A = pm.Uniform("p_A", 0, 1)
+    p_B = pm.Uniform("p_B", 0, 1)
+
+    # Define the deterministic delta function. This is our unknown of interest.
+    delta = pm.Deterministic("delta", p_A - p_B)
+
+    # Set of observations, in this case we have two observation datasets.
+    obs_A = pm.Bernoulli("obs_A", p_A, observed = observations_A)
+    obs_B = pm.Bernoulli("obs_B", p_B, observed = observations_B)
+
+    # To be explained in ch 3.
+    step = pm.Metropolis()
+    trace = pm.sample(20000, step = step)
+    burned_trace=trace[1000:]
+
+p_A_samples = burned_trace["p_A"]
+p_B_samples = burned_trace["p_B"]
+delta_samples = burned_trace["delta"]
+
+figsize(12.5, 10)
+
+# histogram of posteriors
+
+ax = plt.subplot(311)
+
+plt.xlim(0, .1)
+plt.hist(p_A_samples, histtype='stepfilled', bins = 25, alpha = .85,
+        label = "posterior of $p_A$", color = "#A60628", normed=True)
+plt.vlines(true_p_A, 0, 80, linestyle = "--", label = "true $p_A (unknown)")
+plt.legend(loc="upper right")
+plt.title("Posterior distributions of $p_A$, $p_B$, and delta unknowns")
+
+ax = plt.subplot(312)
+
+plt.xlim(0, .1)
+plt.hist(p_B_samples, histtype='stepfilled', bins=25, alpha =.85,
+        label="posterior of $p_B$", color="#467821", normed=True)
+plt.vlines(true_p_B, 0, 80, linestyle="--", label="true $p_B$ (unkown)")
+plt.legend(loc="upper right")
+
+ax = plt.subplot(313)
+plt.hist(delta_samples, histtype='stepfilled', bins=30, alpha=.85,
+        label="posterior of delta", color="#7A68A6", normed=True)
+plt.vlines(true_p_A - true_p_B, 0, 60, linestyle="--",
+            label="true delta (unknown)")
+plt.vlines(0, 0, 60, color="black", alpha=.2)
+plt.legend(loc="upper right")
+
+# Count the number of samples less than 0, i.e. the area under the curve
+# before 0, represent the probability that site A is worse than B
+print("Probability site A is WORSE than site B: %.3f" % \
+    np.mean(delta_samples < 0))
+print("Probability site A is BETTER than site B: %.3f" % \
+    np.mean(delta_samples > 0))
+
+# %% codecell
+# An algorithm for human deceitt
+figsize(12.5, 4)
+
+import scipy.stats as stats
+binomial = stats.binom
+
+parameters = [(10, .4), (10, .9)]
+colors =["#348ABD", "#A60628"]
+
+for i in range(2):
+    N, p = parameters[i]
+    _x = np.arange(N + 1)
+    plt.bar(_x -.5, binomial.pmf(_x, N, p), color=colors[i],
+        edgecolor=colors[i],
+        alpha=.6,
+        label="$N$: %d, $p$: %.1f" % (N, p),
+        linewidth=3)
+
+plt.legend(loc="upper left")
+plt.xlim(0, 10.5)
+plt.xlabel("$k$")
+plt.ylabel("$P(X = k)$")
+plt.title("Probability mass distributions of binomial random variables")
+
+# cheating students
+import pymc3 as pm
+
+N = 100
+with pm.Model() as model:
+    p = pm.Uniform("freq_cheating", 0, 1)
+
+with model:
+    true_answers = pm.Bernoulli("truths", p, shape = N, testval = np.random.binomial(1, .5, N))
