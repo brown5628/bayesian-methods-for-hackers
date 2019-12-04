957ac91eba208826ea94baa9d0aaf5aca9f2bbc9
wbrown <wbrown@yearup.org>
2019-12-04 16:39:44 -0500

    Continue ch 4 to Kaggle Census example.

--- 04_the_greatest_theorem_never_told.py
+++ 04_the_greatest_theorem_never_told.py
@@ -59,3 +59,42 @@ plt.title("How 'fast' is the sample average converging?")
 
 N = 10000
 print( np.mean( [ np.random.exponential( 0.5 ) > 5 for i in range(N) ] ) )
+
+# %% codecell
+figsize( 12.5, 4)
+std_height = 15
+mean_height = 150
+
+n_counties = 5000
+pop_generator = np.random.randint
+norm = np.random.normal
+
+#generate some artificial population numbers
+population = pop_generator(100, 1500, n_counties)
+
+average_across_county = np.zeros(n_counties)
+for i in range(n_counties):
+    average_across_county[i] = norm(mean_height, 1./std_height, population[i]).mean()
+
+i_min = np.argmin(average_across_county)
+i_max = np.argmax(average_across_county)
+
+plt.scatter(population, average_across_county, alpha= .5, c="#7A68A6")
+plt.scatter([population[i_min], population[i_max]],
+            [average_across_county[i_min], average_across_county[i_max]],
+            s = 60, marker = "o", facecolors = "none",
+            edgecolors= "#A60628", linewidths = 1.5,
+            label = "extreme heights")
+
+plt.xlim(100, 1500)
+plt.title("Average height vs. County Population")
+plt.xlabel("County Population")
+plt.ylabel("Average height in country")
+plt.plot([100, 1500], [150, 150], color="k", label = "true expected \ height", ls ="--")
+plt.legend(scatterpoints=1)
+
+
+print("Population sizes of 10 'shortest' counties: ")
+print(population[ np.argsort( average_across_county )[:10] ], '\n')
+print("Population sizes of 10 'tallest' counties: ")
+print(population[ np.argsort( -average_across_county )[:10] ])
