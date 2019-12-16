49e533f2267a69c5c983f25b1f592bec4b46efb0
Will Brown <brown5628@gmail.com>
2019-12-16 07:20:51 -0500

    Complete Ch 5, begin Ch 6 to Gamma.

--- 05_loss_functions.py
+++ 05_loss_functions.py
@@ -101,3 +101,315 @@ plt.legend(loc="upper left", title="Risk parameter")
 plt.xlabel("price bid")
 plt.ylabel("expected loss")
 plt.xlim(5000, 30000)
+
+import scipy.optimize as sop
+
+ax = plt.subplot(111)
+
+for _p in risks:
+    _color = next(ax._get_lines.prop_cycler)
+    _min_results = sop.fmin(expected_loss, 15000, args=(_p,), disp =False)
+    _results = [expected_loss(_g, _p) for _g in guesses]
+    plt.plot(guesses, _results, color = _color['color'])
+    plt.scatter(_min_results, 0, s =60, color = _color['color'], label = "%d"%_p)
+    plt.vlines(_min_results, 0, 120000, color = _color['color'], linestyles="--")
+    print("minimum at risk %d: %.2f" % (_p, _min_results))
+
+plt.title("Expected loss & Bayes actions of different guesses, \n \ various risk-levels of overestimating")
+plt.legend(loc="upper left", scatterpoints = 1, title = "Bayes action at risk:")
+plt.xlabel("price guess")
+plt.ylabel("expected loss")
+plt.xlim(7000, 30000)
+plt.ylim(-1000, 80000)
+
+# %% codecell
+figsize(12.5, 4)
+
+def stock_loss(true_return, yhat, alpha = 100.):
+    if true_return * yhat < 0:
+        return alpha*yhat**2 - np.sign(true_return) * yhat +abs(true_return)
+    else:
+        return abs(true_return - yhat)
+
+true_value = .05
+pred = np.linspace(-.04, .12, 75)
+
+plt.plot(pred, [stock_loss(true_value, _p) for _p in pred], label = "loss associated with\n prediction if true value = .05", lw = 3)
+plt.vlines(0, 0, .25, linestyles="--")
+
+plt.xlabel("prediction")
+plt.ylabel("loss")
+plt.xlim(-.04, .12)
+plt.ylim(0, .25)
+
+true_value = -.02
+plt.plot(pred, [stock_loss(true_value, _p) for _p in pred], alpha = .6, label = "Loss associated with\n prediction if true value = -.02", lw = 3)
+plt.legend()
+plt.title("Stock returns loss if true value = .05, -.02")
+
+N = 100
+X = .025*np.random.randn(N)
+Y = .5*X + .01*np.random.randn(N)
+
+ls_coef_ = np.cov(X, Y)[0, 1]/np.var(X)
+ls_intercept = Y.mean() - ls_coef_*X.mean()
+
+plt.scatter(X, Y, c= "k")
+plt.xlabel("trading signal")
+plt.ylabel("returns")
+plt.title("Empirical returns vs. trading signal")
+plt.plot(X, ls_coef_*X + ls_intercept, label = "Least-squares line")
+plt.xlim(X.min(), X.max())
+plt.ylim(Y.min(), Y.max())
+plt.legend(loc = "upper left")
+
+import pymc3 as pm
+
+with pm.Model() as model:
+    std = pm.Uniform("std", 0, 100)
+
+    beta = pm.Normal("beta", mu=0, sd=100)
+    alpha = pm.Normal("alpha", mu=0, sd=100)
+
+    mean = pm.Deterministic("mean", alpha + beta*X)
+
+    obs = pm.Normal("obs", mu=mean, sd=std, observed=Y)
+
+    trace = pm.sample(100000, step=pm.Metropolis())
+    burned_trace = trace[20000:]
+
+pm.plots.traceplot(trace=burned_trace, varnames=["std", "beta", "alpha"])
+pm.plot_posterior(trace=burned_trace, varnames=["std", "beta", "alpha"], kde_plot=True)
+
+figsize(12.5, 6)
+from scipy.optimize import fmin
+
+def stock_loss(price, pred, coef = 500):
+    """vectorized for numpy"""
+    sol = np.zeros_like(price)
+    ix = price*pred < 0
+    sol[ix] = coef*pred**2 - np.sign(price[ix])*pred+abs(price[ix])
+    sol[~ix] = abs(price[~ix] - pred)
+    return sol
+
+std_samples = burned_trace["std"]
+alpha_samples = burned_trace["alpha"]
+beta_samples = burned_trace["beta"]
+
+N = std_samples.shape[0]
+
+noise = std_samples*np.random.randn(N)
+
+possible_outcomes = lambda signal: alpha_samples + beta_samples*signal + noise
+
+opt_predictions =  np.zeros(50)
+trading_signals = np.linspace(X.min(), X.max(), 50)
+for i, _signal in enumerate(trading_signals):
+    _possible_outcomes = possible_outcomes(_signal)
+    tomin = lambda pred: stock_loss(_possible_outcomes, pred).mean()
+    opt_predictions[i] = fmin(tomin, 0, disp = False)
+
+plt.xlabel("trading signal")
+plt.ylabel("prediction")
+plt.title("Least-squares prediction vs. Bayes action prediction")
+plt.plot(X, ls_coef_*X + ls_intercept, label = "Least-squares prediction")
+plt.xlim(X.min(), X.max())
+plt.plot(trading_signals, opt_predictions, label = "Bayes action prediction")
+plt.legend(loc="upper left")
+
+
+# %% codecell
+from draw_sky2 import draw_sky
+
+n_sky = 3 #choose a file/sky to examine.
+data = np.genfromtxt("data/Train_Skies/Train_Skies/\
+Training_Sky%d.csv" % (n_sky),
+                      dtype = None,
+                      skip_header = 1,
+                      delimiter = ",",
+                      usecols = [1,2,3,4])
+print("Data on galaxies in sky %d."%n_sky)
+print("position_x, position_y, e_1, e_2 ")
+print(data[:3])
+
+fig = draw_sky(data)
+plt.title("Galaxy positions and ellipcities of sky %d." % n_sky)
+plt.xlabel("x-position")
+plt.ylabel("y-position")
+
+import pymc3 as pm
+import theano.tensor as T
+
+def euclidean_distance(x, y):
+    return np.sqrt(((x - y)**2)).sum(axis=1)
+
+def f_distance(gxy_pos, halo_pos, c):
+    # foo_position should be a 2-d numpy array
+    # T.maximum() provides our element-wise maximum as in NumPy, but instead for theano tensors
+    return T.maximum(euclidean_distance(gxy_pos, halo_pos), c)[:, None]
+
+def tangential_distance(glxy_position, halo_position):
+    # foo_position should be a 2-d numpy array
+    delta = glxy_position - halo_position
+    t = (2*T.arctan(delta[:,1]/delta[:,0]))
+    return T.stack([-T.cos(t), -T.sin(t)], axis=1)
+
+
+with pm.Model() as model:
+    #set the size of the halo's mass
+    mass_large = pm.Uniform("mass_large", 40, 180)
+
+    #set the initial prior position of the halos, it's a 2-d Uniform dist.
+    halo_position = pm.Uniform("halo_position", 0, 4200, shape=(1,2))
+
+    mean = pm.Deterministic("mean", mass_large /\
+            f_distance(T.as_tensor(data[:,:2]), halo_position, 240)*\
+            tangential_distance(T.as_tensor(data[:,:2]), halo_position))
+
+    ellpty = pm.Normal("ellipcity", mu=mean, tau=1./0.05, observed=data[:,2:])
+
+with model:
+    mu, sds, elbo = pm.variational.advi(n=50000)
+    step = pm.NUTS(scaling=model.dict_to_array(sds), is_cov=True)
+    trace = pm.sample(5000, step=step, start=mu)
+
+t = trace["halo_position"].reshape(5000,2)
+
+fig = draw_sky(data)
+plt.title("Galaxy positions and ellipcities of sky %d." % n_sky)
+plt.xlabel("x-position")
+plt.ylabel("y-position")
+plt.scatter(t[:,0], t[:,1], alpha = 0.015, c = "r")
+plt.xlim(0, 4200)
+plt.ylim(0, 4200)
+
+
+halo_data = np.genfromtxt("data/Training_halos.csv",
+                          delimiter = ",",
+                          usecols = [1, 2,3, 4,5,6,7,8,9],
+                          skip_header = 1)
+print(halo_data[n_sky])
+
+fig = draw_sky(data)
+plt.title("Galaxy positions and ellipcities of sky %d." % n_sky)
+plt.xlabel("x-position")
+plt.ylabel("y-position")
+plt.scatter(t[:,0], t[:,1], alpha = 0.015, c = "r")
+plt.scatter(halo_data[n_sky-1][3], halo_data[n_sky-1][4],
+            label = "True halo position",
+            c = "k", s = 70)
+plt.legend(scatterpoints = 1, loc = "lower left")
+plt.xlim(0, 4200)
+plt.ylim(0, 4200);
+
+print("True halo location:", halo_data[n_sky][3], halo_data[n_sky][4])
+
+mean_posterior = t.mean(axis=0).reshape(1,2)
+print(mean_posterior)
+
+
+from DarkWorldsMetric import main_score
+
+_halo_data = halo_data[n_sky-1]
+
+nhalo_all =  _halo_data[0].reshape(1,1)
+x_true_all = _halo_data[3].reshape(1,1)
+y_true_all = _halo_data[4].reshape(1,1)
+x_ref_all = _halo_data[1].reshape(1,1)
+y_ref_all = _halo_data[2].reshape(1,1)
+sky_prediction = mean_posterior
+
+print("Using the mean:")
+main_score(nhalo_all, x_true_all, y_true_all, \
+            x_ref_all, y_ref_all, sky_prediction)
+
+#what's a bad score?
+random_guess = np.random.randint(0, 4200, size=(1,2))
+print("Using a random location:", random_guess)
+main_score(nhalo_all, x_true_all, y_true_all, \
+            x_ref_all, y_ref_all, random_guess)
+
+def halo_posteriors(n_halos_in_sky, galaxy_data,samples = 5e5, burn_in = 500):
+    #set the size of the halo's mass
+    with pm.Model() as model:
+        mass_large = pm.Uniform("mass_large", 40, 180)
+
+        mass_small_1 = 20
+        mass_small_2 = 20
+
+        masses = np.array([mass_large,mass_small_1, mass_small_2], dtype=object)
+
+        #set the initial prior positions of the halos, it's a 2-d Uniform dist.
+        halo_positions = pm.Uniform("halo_positions", 0, 4200, shape=(n_halos_in_sky,2)) #notice this size
+
+        fdist_constants = np.array([240, 70, 70])
+
+        _sum = 0
+        for i in range(n_halos_in_sky):
+            _sum += masses[i]/f_distance(data[:,:2], halo_positions[i, :], fdist_constants[i])*\
+                tangential_distance(data[:,:2], halo_positions[i, :])
+
+        mean = pm.Deterministic("mean", _sum)
+
+        ellpty = pm.Normal("ellipcity", mu=mean, tau=1./0.05, observed=data[:,2:])
+
+        mu, sds, elbo = pm.variational.advi(n=50000)
+        step = pm.NUTS(scaling=model.dict_to_array(sds), is_cov=True)
+        trace = pm.sample(samples, step=step, start=mu)
+
+    burned_trace = trace[burn_in:]
+    return burned_trace["halo_positions"]
+
+n_sky = 215
+data = np.genfromtxt("data/Train_Skies/Train_Skies/\
+Training_Sky%d.csv" % (n_sky),
+                      dtype = None,
+                      skip_header = 1,
+                      delimiter = ",",
+                      usecols = [1,2,3,4])
+
+samples = 5000
+traces = halo_posteriors(3, data, samples = samples, burn_in=500)
+
+fig = draw_sky(data)
+plt.title("Galaxy positions and ellipcities of sky %d." % n_sky)
+plt.xlabel("x-position")
+plt.ylabel("y-position")
+
+colors = ["#467821", "#A60628", "#7A68A6"]
+
+for i in range(traces.shape[1]):
+    plt.scatter(traces[:, i, 0],  traces[:, i, 1], c = colors[i], alpha = 0.02)
+
+
+for i in range(traces.shape[1]):
+    plt.scatter(halo_data[n_sky-1][3 + 2*i], halo_data[n_sky-1][4 + 2*i],
+            label = "True halo position", c = "k", s = 90)
+
+#plt.legend(scatterpoints = 1)
+plt.xlim(0, 4200)
+plt.ylim(0, 4200)
+
+print(traces.shape)
+
+mean_posterior = traces.mean(axis=0).reshape(1,6)
+print(mean_posterior)
+
+
+nhalo_all =  _halo_data[0].reshape(1,1)
+x_true_all = _halo_data[3].reshape(1,1)
+y_true_all = _halo_data[4].reshape(1,1)
+x_ref_all = _halo_data[1].reshape(1,1)
+y_ref_all = _halo_data[2].reshape(1,1)
+sky_prediction = mean_posterior
+
+
+print("Using the mean:")
+main_score([1], x_true_all, y_true_all, \
+            x_ref_all, y_ref_all, sky_prediction)
+
+#what's a bad score?
+random_guess = np.random.randint(0, 4200, size=(1,2))
+print("Using a random location:", random_guess)
+main_score([1], x_true_all, y_true_all, \
+            x_ref_all, y_ref_all, random_guess)
