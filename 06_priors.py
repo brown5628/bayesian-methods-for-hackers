ffc2e384c4769458094ed2bf335d19c85835836f
Will Brown <brown5628@gmail.com>
2019-12-17 07:20:30 -0500

    Continue Ch 6 to Expert Prior

--- 06_priors.py
+++ 06_priors.py
@@ -26,3 +26,231 @@ plt.ylim(0, 4)
 leg = plt.legend(loc = "upper left")
 leg.get_frame().set_alpha(.4)
 plt.title("Comparing objective vs. subjective priors for an unknown probability")
+
+# %% codecell
+# Gamma
+figsize(12.5, 5)
+gamma = stats.gamma
+
+parameters = [(1, .5), (9, 2), (3, .5), (7, .5)]
+x = np.linspace(.001, 20, 150)
+for alpha, beta in parameters:
+    y = gamma.pdf(x, alpha, scale=1./beta)
+    lines = plt.plot(x, y, label = "(%.1f,%.1f)"%(alpha,beta), lw =3)
+    plt.fill_between(x, 0, y, alpha = .2, color = lines[0].get_color())
+    plt.autoscale(tight = True)
+
+plt.legend(title=r"$\alpha, \beta$ - parameters")
+
+# Wishart
+n = 4
+for i in range(10):
+    ax = plt.subplot(2, 5, i+1)
+    if i >= 5:
+        n = 15
+    plt.imshow(stats.wishart.rvs(n+1, np.eye(n)), interpolation="none", cmap="hot")
+    ax.axis("off")
+
+plt.suptitle("Random matrices form a Wishart Distribution")
+
+# Beta
+figsize(12.5, 5)
+
+params = [(2, 5), (1, 1), (.5, .5), (5, 5), (20, 4), (5, 1)]
+
+x = np.linspace(.01, .99, 100)
+beta = stats.beta
+for a, b in params:
+    y = beta.pdf(x, a, b)
+    lines = plt.plot(x, y, label = "(%.1f, %.1f)"%(a,b), lw = 3)
+    plt.fill_between(x, 0, y, alpha = .2, color = lines[0].get_color())
+    plt.autoscale(tight=True)
+plt.ylim(0)
+plt.legend(loc='upper left', title='(a,b)-parameters')
+
+# BANDITS
+rand = np.random.rand
+
+class Bandits(object):
+    """
+    This class represents N bandits machines.
+    parameters:
+        p_array: a (n,) Numpy array of probabilities > 0, <1.
+    methods:
+        pull( i ): return the results, 0 or 1, of pulling the ith bandit.
+    """
+    def __init__(self, p_array):
+        self.p = p_array
+        self.optimal = np.argmax(p_array)
+
+    def pull(self, i):
+        #i is which arm to pull
+        return np.random.rand() < self.p[i]
+
+    def __len__(self):
+        return len(self.p)
+
+class BayesianStrategy(object):
+    """
+    Implements a online, learning strategy to solve
+    the Multi-Armed Bandit problem.
+
+    parameters:
+        bandits: a Bandit class with .pull method
+
+    methods:
+        sample_bandits(n): sample and train on n pulls.
+
+    attributes:
+        N: the cumulative number of samples
+        choices: the historical choices as a (N,) array
+        bb_score: the historical score as a (N,) array
+    """
+
+    def __init__(self, bandits):
+
+        self.bandits = bandits
+        n_bandits = len(self.bandits)
+        self.wins = np.zeros(n_bandits)
+        self.trials = np.zeros(n_bandits)
+        self.N = 0
+        self.choices = []
+        self.bb_score = []
+
+    def sample_bandits(self, n=1):
+
+        bb_score = np.zeros(n)
+        choices = np.zeros(n)
+
+        for k in range(n):
+            # sample from the bandit's priors, and select the largest sample
+            choice = np.argmax(np.random.beta(1 + self.wins, 1 + self.trials - self.wins))
+
+            # sample the chosen bandit
+            result = self.bandits.pull(choice)
+
+            # update priors and score
+            self.wins[choice] += result
+            self.trials[choice] +=1
+            bb_score[k] = result
+            self.N += 1
+            choices[k] - choice
+
+        self.bb_score = np.r_[self.bb_score, bb_score]
+        self.choices = np.r_[self.choices, choices]
+        return
+
+figsize(11.0, 10)
+
+beta = stats.beta
+x = np.linspace(.001, .999, 200)
+
+def plot_priors(bayesian_strategy, prob, lw = 3, alpha = .2, plt_vlines = True):
+    ## plotting function
+    wins = bayesian_strategy.wins
+    trials = bayesian_strategy.trials
+    for i in range(prob.shape[0]):
+        y = beta(1+wins[i], 1+trials[i]-wins[i])
+        p = plt.plot(x, y.pdf(x), lw=lw)
+        c = p[0].get_markeredgecolor()
+        plt.fill_between(x,y.pdf(x),0, color = c, alpha = alpha, label = "underlying probability: %.2f" % prob[i])
+
+        if plt_vlines:
+            plt.vlines(prob[i], 0, y.pdf(prob[i]), colors = c, linestyles = "--", lw = 2)
+        plt.autoscale(tight = "True")
+        plt.title("Posteriors After %d pull" % bayesian_strategy.N +\
+                    "s"*(bayesian_strategy.N > 1))
+        plt.autoscale(tight = True)
+    return
+
+hidden_prob = np.array([.85, .6, .75])
+bandits = Bandits(hidden_prob)
+bayesian_strat = BayesianStrategy(bandits)
+
+draw_samples = [1, 1, 3, 10, 10, 25, 50, 100, 200, 600]
+
+for j,i in enumerate(draw_samples):
+    plt.subplot(5, 2, j+1)
+    bayesian_strat.sample_bandits(i)
+    plot_priors(bayesian_strat, hidden_prob)
+    #plt.legend()
+    plt.autoscale(tight=True)
+plt.tight_layout()
+
+
+figsize(12.5, 5)
+from other_strats import *
+
+# define a harder problem
+hidden_prob = np.array([.15, .2, .1, .05])
+bandits = Bandits(hidden_prob)
+
+# define regret
+def regret(probabilities, choices):
+    w_opt = probabilities.max()
+    return (w_opt - probabilities[choices.astype(int)]).cumsum()
+
+# create new strategies
+strategies = [upper_credible_choice,
+                bayesian_bandit_choice,
+                ucb_bayes,
+                max_mean,
+                random_choice]
+algos = []
+for strat in strategies:
+    algos.append(GeneralBanditStrat(bandits, strat))
+
+# train 10000 times
+for strat in algos:
+    strat.sample_bandits(10000)
+
+# test and plot
+for i,strat in enumerate(algos):
+    _regret = regret(hidden_prob, strat.choices)
+    plt.plot(_regret, label = strategies[i].__name__, lw = 3)
+
+plt.title("Total Regret of Bayesian Bandits Strategy vs. Random guessing")
+plt.xlabel("Number of pulls")
+plt.ylabel("Regret after $n$ pulls")
+plt.legend(loc = "upper left")
+
+trials = 500
+expected_total_regret = np.zeros((10000, 3))
+
+for i_strat, strat in enumerate(strategies[:-2]):
+    for i in range(trials):
+        general_strat = GeneralBanditStrat(bandits, strat)
+        general_strat.sample_bandits(10000)
+        _regret =  regret(hidden_prob, general_strat.choices)
+        expected_total_regret[:,i_strat] += _regret
+    plt.plot(expected_total_regret[:,i_strat]/trials, lw =3, label = strat.__name__)
+
+plt.title("Expected Total Regret of Multi-armed Bandit strategies")
+plt.xlabel("Number of pulls")
+plt.ylabel("Exepected Total Regret \n after $n$ pulls");
+plt.legend(loc = "upper left")
+
+plt.figure()
+[pl1, pl2, pl3] = plt.plot(expected_total_regret[:, [0,1,2]], lw = 3)
+plt.xscale("log")
+plt.legend([pl1, pl2, pl3],
+           ["Upper Credible Bound", "Bayesian Bandit", "UCB-Bayes"],
+            loc="upper left")
+plt.ylabel("Exepected Total Regret \n after $\log{n}$ pulls");
+plt.title( "log-scale of above" );
+plt.ylabel("Exepected Total Regret \n after $\log{n}$ pulls")
+
+
+figsize(12.0, 8)
+beta = stats.beta
+hidden_prob = beta.rvs(1,13, size = 35)
+print(hidden_prob)
+bandits = Bandits(hidden_prob)
+bayesian_strat = BayesianStrategy(bandits)
+
+for j,i in enumerate([100, 200, 500, 1300]):
+    plt.subplot(2, 2, j+1)
+    bayesian_strat.sample_bandits(i)
+    plot_priors(bayesian_strat, hidden_prob, lw = 2, alpha = 0.0, plt_vlines=False)
+    #plt.legend()
+    plt.xlim(0, 0.5)
